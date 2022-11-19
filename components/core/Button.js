dc288db3365149fc293af59bb13c88a2b9972346
Daniel Schindler <client55@protonmail.com>
2022-11-19 15:29:16 +0100

    Add front page.

---
+++ components/core/Button.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Button(props) {
+  return (
+    <button
+      className="h-min flex justify-center items-center rounded py-1 px-3 cursor-pointer bg-gray-100 shadow-outer transition-all"
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      <div className="">{props.children}</div>
+    </button>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
-import { ConnectKitButton } from "connectkit";
+import Apply from "/components/main/Apply";
 
-export default function HomePage() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <ConnectKitButton />
-    </div>
-  );
+export default function ApplyPage(context) {
+  return <Apply />;
 }
-
-// import Apply from "/components/main/Apply";
-
-// export default function ApplyPage(context) {
-//   return <Apply />;
-// }
