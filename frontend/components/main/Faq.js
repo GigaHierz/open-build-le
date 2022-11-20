f14bbd7cdbcae93885e64e081f9ade8bce0b28ad
Daniel Schindler <client55@protonmail.com>
2022-11-20 13:33:08 +0100

    Remove applied. Change reimburese, faq styles.

--- frontend/components/main/Faq.js
+++ frontend/components/main/Faq.js
@@ -1,32 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "/public/images/logo.png";
 import YoutubeEmbed from "/components/core/YoutubeEmbed";
-import Button from "/components/core/Button";
 
-export default function Apply(props) {
-  const [privacyPolicy, setprivacyPolicy] = useState(false);
-  const [terms, setTerms] = useState(false);
-
-  function withdraw() {
-    console.log("withdraw");
-  }
-
+export default function FAQ(props) {
   return (
-    <div className="p-4 xl:p-16 bg-grey">
-      <header>
-        <Image src={Logo} alt="Logo of H.E.R. DAO" />
-        <a>about</a>
-        <a>apply</a>
-        <a>reimburse</a>
-        <a>FAQ</a>
-      </header>
-
-      <h2>FAQ</h2>
-      <div className="grid grid-cols-2 gap-4">
+    <div className="grid justify-items-center">
+      <h1>FAQ</h1>
+      <div className="grid gap-4 justify-items-center">
         <div>
           <h2>How to secure a ticket</h2>
           <p>
@@ -47,17 +27,19 @@ export default function Apply(props) {
             more!
           </p>
 
-          <div className="App">
+          <div className="grid justify-items-center">
             <h1>Youtube Embed</h1>
             <YoutubeEmbed youtubeId="ZE2HxTmxfrI" />
           </div>
-          <h2>How long will it take to create your first wallet?</h2>
-          <p>It will take about 45 minutes to 1 hour to do this.</p>
-          <h2>Which browser can I just for this</h2>
-          <p>
-            Google Chrome is the best if you choose a browser. Otherwise,
-            install the MetaMask
-          </p>
+          <div className="grid justify-items-center">
+            <h2>How long will it take to create your first wallet?</h2>
+            <p>It will take about 45 minutes to 1 hour to do this.</p>
+            <h2>Which browser can I just for this</h2>
+            <p>
+              Google Chrome is the best if you choose a browser. Otherwise,
+              install the MetaMask
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- frontend/components/main/Reimburse.js
+++ frontend/components/main/Reimburse.js
@@ -1,76 +1,74 @@
-'use client'
+"use client";
 
 import {
   usePrepareContractWrite,
   useContractWrite,
-  useWaitForTransaction
-} from 'wagmi'
+  useWaitForTransaction,
+} from "wagmi";
 
-import Button from '/components/core/Button'
+import Button from "/components/core/Button";
 
-export default function Reimburse (props) {
+export default function Reimburse(props) {
   const {
     config,
     error: prepareError,
-    isError: isPrepareError
+    isError: isPrepareError,
   } = usePrepareContractWrite({
-    addressOrName: '0xae5c8A495486fC0E14c6833f5772018976c5cD9a',
+    addressOrName: "0xae5c8A495486fC0E14c6833f5772018976c5cD9a",
     contractInterface: [
       {
         inputs: [],
-        name: 'withdraw',
+        name: "withdraw",
 
         outputs: [
           {
-            internalType: 'string',
-            name: '',
-            type: 'string'
-          }
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
         ],
-        stateMutability: 'nonpayable',
-        type: 'function'
-      }
+        stateMutability: "nonpayable",
+        type: "function",
+      },
     ],
-    functionName: 'withdraw'
-  })
-  const { data, error, isError, write } = useContractWrite(config)
+    functionName: "withdraw",
+  });
+  const { data, error, isError, write } = useContractWrite(config);
   const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash
-  })
+    hash: data?.hash,
+  });
 
   return (
     <div>
-      <h1>Get your Stake back</h1>
-      <div className='grid grid-cols-2 gap-4'>
-        <div>
-          <div className='flex gap-4'>
-            <p>
-              Womenhack NY 2023 was a huge success and we are so glad you could
-              share this beautiful experience with us. If you hold the two
-              requested POAPs, you will receive your staked tokens back.
-            </p>
-            <p className='mt-8'>
-              If you hold the two requested POAPs, you will receive your staked
-              tokens back.{' '}
-            </p>
-            <Button disabled={!write || isLoading} onClick={() => write()}>
-              Reimburse
-            </Button>
+      <h1 className="text-center">Get your Stake back</h1>
+      <div className="grid grid-cols-2 gap-8">
+        <p>
+          Womenhack NY 2023 was a huge success and we are so glad you could
+          share this beautiful experience with us. If you hold the two requested
+          POAPs, you will receive your staked tokens back.
+        </p>
+        <p>
+          If you hold the two requested POAPs, you will receive your staked
+          tokens back.{" "}
+        </p>
+      </div>
+      <div className="flex justify-center mt-8">
+        <Button disabled={!write || isLoading} onClick={() => write()}>
+          Reimburse
+        </Button>
 
-            {/* TODO: pop-up for error message  */}
-            {(isPrepareError || isError) && (
-              <div>
-                {' '}
-                {(prepareError || error)?.message?.includes(
-                  'You already got reimbursed'
-                )
-                  ? 'You already got reimbursed'
-                  : error?.message}
-              </div>
-            )}
+        {/* TODO: pop-up for error message  */}
+        {(isPrepareError || isError) && (
+          <div>
+            {" "}
+            {(prepareError || error)?.message?.includes(
+              "You already got reimbursed"
+            )
+              ? "You already got reimbursed"
+              : error?.message}
           </div>
-        </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
