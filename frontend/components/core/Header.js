f64eeda8c62ccd357745896c74af646a300be9b4
Daniel Schindler <client55@protonmail.com>
2022-11-20 11:36:07 +0100

    Add Header.

--- frontend/components/core/Header.js
+++ frontend/components/core/Header.js
@@ -1,16 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "/public/images/logo.png";
 
 export default function Header(props) {
   return (
-    <header>
+    <header className="h-full flex justify-between">
       <Image src={Logo} alt="Logo of H.E.R. DAO" />
-      <Link href="/ab"></Link>
-      <a>about</a>
-      <a>apply</a>
-      <a>reimburse</a>
-      <a>FAQ</a>
+
+      <div className="flex items-end gap-6 text-lg">
+        <Link href="/about">
+          <a>about</a>
+        </Link>
+        <Link href="/apply">
+          <a>apply</a>
+        </Link>
+        <Link href="/reimburse">
+          <a>reimburse</a>
+        </Link>
+        <Link href="/faq">
+          <a>FAQ</a>
+        </Link>
+      </div>
     </header>
   );
 }
